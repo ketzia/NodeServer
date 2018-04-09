f672f1092b6965f17ef0e9ac0e29ffd24930b7c3
Ketzia Dante <ketzia.dante@gmail.com>
2018-04-09 09:29:31 -0500

    Starting server

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const methodOverride    = require('method-override');
+const PORT = process.env.PORT || 8080;
+
+app.all('*', function(req,res,next){
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers","Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+   next();
+});
+app.use(methodOverride("X-HTTP-Method-Override"));
+//routes(app);
+
+app.listen(PORT,()=>{
+    console.log('Server running on Port '+ PORT);
+});
